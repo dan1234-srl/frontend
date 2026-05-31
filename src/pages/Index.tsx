3699d2e42133351fd1d67d13bb23bd9e079df593
IvanFloreaAlexandru <147402164+IvanFloreaAlexandru@users.noreply.github.com>
2026-05-31 16:39:05 +0300

    ee

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -45,7 +45,7 @@ const Index = () => {
       <Header />
 
       {/* Spațiu controlat prin padding pe main, fără elemente spacer inutile */}
-      <main className="flex-1 w-full relative pt-36 lg:pt-44">
+      <main className="flex-1 w-full relative pt-32 lg:pt-40">
         {/* CONTAINER 90% (max 1700px), gap redus la 8-12 pentru densitate */}
         <div className="w-[90%] max-w-[1700px] mx-auto flex flex-col gap-8 md:gap-12 pb-20">
           <motion.div
